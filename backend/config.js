f7501997ab17beb505e44e9d173291e7907e22bf
KyleHubbs008 <khubb008@odu.edu>
2024-03-17 11:39:43 -0400

    fixed mongoDBURL

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,3 @@
 export const PORT = 5555;
 
-export const mongoDBURL = 'imkyle1988:tLdpw8JO5G2NJe8F@bookstore-mern.woojxqo.mongodb.net/books-collection?retryWrites=true&w=majority'
+export const mongoDBURL = 'mongodb+srv://imkyle1988:tLdpw8JO5G2NJe8F@bookstore-mern.woojxqo.mongodb.net/books-collection?retryWrites=true&w=majority'
