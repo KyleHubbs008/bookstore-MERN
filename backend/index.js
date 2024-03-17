4a519a9ba7362db1cd038c27565d575093f97a1e
KyleHubbs008 <khubb008@odu.edu>
2024-03-17 17:30:50 -0400

    Created an HTTP route for getting a specific book from the database using the book's ID

--- backend/index.js
+++ backend/index.js
@@ -42,12 +42,27 @@ app.post('/books', async (request, response) => {
 app.get('/books', async (request, response) => {
     try {
         const books = await Book.find({});
-        return response.status(200).json(books);
+        return response.status(200).json({
+            count: books.length,
+            data: books
+        });
     } catch (error) {
         console.log(error.message);
         response.status(500).send({ message: error.message});
     }
-})
+});
+
+//Route for getting a specific book from the database by ID
+app.get('/books/:id', async (request, response) => {
+    try {
+        const { id } = request.params;
+        const book = await Book.findById(id);
+        return response.status(200).json(book);
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({ message: error.message});
+    }
+});
 
 mongoose
     .connect(mongoDBURL)
